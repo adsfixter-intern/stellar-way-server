9077fd73e2ae87811ddf21882d3d1a5eeffed6e0
omar-faruk72 <mdfaruk01720875872@gmail.com>
2026-03-14 11:57:32 +0600

    faq all api successfully working

--- src/modules/faq/faq.routes.ts
+++ src/modules/faq/faq.routes.ts
@@ -4,6 +4,8 @@ import { FaqControllers } from './faq.controller';
 const router = Router();
 
 router.post('/create-faq', FaqControllers.createFaq);
-// router.get('/', FaqControllers.getAllFaqs);
-
+router.get('/all-faq', FaqControllers.getAllFaqs);
+router.get("/faq/:id", FaqControllers.getSingleFaq);
+router.put("/faq/:id", FaqControllers.updateFaq);
+router.delete("/faq/:id", FaqControllers.deleteFaq);
 export const FaqRoutes = router;
