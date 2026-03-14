28fc91a0832906cd64cf1f8922ff22a0896aa0ea
Hasan Rafi Ahmed <hasan.cse570@gmail.com>
2026-03-14 12:10:05 +0600

    notify controller route add

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,10 +1,12 @@
-import { Router } from 'express';
-import { MenuRoutes } from '../../modules/menu/menu.route';
+import { Router } from "express";
+import { MenuRoutes } from "../../modules/menu/menu.route";
+import { NotificationRoutes } from "../../modules/notification/notification.route";
 
 const router = Router();
 
 const moduleRoutes = [
-  { path: '/menu', route: MenuRoutes },
+  { path: "/menu", route: MenuRoutes },
+  { path: "/notifications",route: NotificationRoutes },
 ];
 
 moduleRoutes.forEach((route) => router.use(route.path, route.route));
